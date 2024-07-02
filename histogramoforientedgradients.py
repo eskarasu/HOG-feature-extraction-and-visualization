dd21ecb305ae61cec0365b163c02b61687ae215b
Eyüp Said Karasu <126612347+eskarasu@users.noreply.github.com>
2024-07-02 12:25:08 +0300

    Comment rows translated to English
    Comment rows translated to English

--- histogramoforientedgradients.py
+++ histogramoforientedgradients.py
@@ -1,14 +1,14 @@
 from skimage import feature, io
 import matplotlib.pyplot as plt
 
-# Görüntüyü okuyalım
+# Read the image
 image = io.imread('image.png', as_gray=True)
 
-# HOG özelliklerini çıkaralım
+# Extraction of HOG feature
 hog_features, hog_image = feature.hog(image, orientations=8, pixels_per_cell=(16, 16),
                                       cells_per_block=(1, 1), visualize=True)
 
-# Orijinal görüntüyü ve HOG görüntüsünü çizdirelim
+# Plot the original image and the HOG image
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
 
 ax1.axis('off')
